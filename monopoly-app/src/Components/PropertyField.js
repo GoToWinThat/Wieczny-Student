8e12c30004993652b523b0c974bbdd00bfcdac1a
Dariusz <dariusz.momot98@gmail.com>
2020-12-30 14:20:40 +0100

    Hover effect on property fields
    more information on property fields hover effect

--- monopoly-app/src/Components/PropertyField.js
+++ monopoly-app/src/Components/PropertyField.js
@@ -14,14 +14,23 @@ class PropertyField extends Component {
             <Popover id="popover-card">
                 <Popover.Content>
                     <div className="card card-popover">
-                            <div className="card-header" style={{backgroundColor:`${this.props.data.color}`}}></div>
+                            <div className="card-header" style={{backgroundColor:`${this.props.data.color}`, fontSize: "0.5em"}}>
+                                {this.name}
+                            </div>
                             <div className="card-body">
-                            <div className="card-title">{this.ShortenWords()}</div>
-                            <div className="card-text">{this.props.data.price} ECTS</div>
+                            <div className="card-title">Cena {this.props.data.price} ECTS</div>
+                                <p className="card-text card-text-rentCost">Czynsz {this.props.data.rentCosts[0]} ECTS</p>
+                                <p className="card-text card-text-rentCost">Z jednym PC {this.props.data.rentCosts[1]} ECTS</p>
+                                <p className="card-text card-text-rentCost">Z dwoma PC {this.props.data.rentCosts[2]} ECTS</p>
+                                <p className="card-text card-text-rentCost">Z trzema PC {this.props.data.rentCosts[3]} ECTS</p>
+                                <p className="card-text card-text-rentCost">Z serwerem {this.props.data.rentCosts[4]} ECTS</p>
+                                <p className="card-text card-text-rentCost">Koszt PC {this.props.data.estatePrice} ECTS</p>
+                                <p className="card-text card-text-rentCost">Zastaw {this.props.data.mortgage} ECTS</p>
                         </div>
                     </div>
                 </Popover.Content>
             </Popover>
+
         )
     }
 
